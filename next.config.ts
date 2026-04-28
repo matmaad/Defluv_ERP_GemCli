2fbfc5ccec09b73a2ee32e27ad57d8ed82420499
matmaad <matmaad@gmail.com>
2026-04-27 20:51:12 -0400

    chore: trigger preview deployment for vercel checklist

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Triggering preview deployment for Vercel checklist
 };
 
 export default nextConfig;
